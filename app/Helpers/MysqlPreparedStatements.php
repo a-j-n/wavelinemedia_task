fbf7bb30b4f6d539c2e2524ebb3b5892393939d4
a-j-n <e.a.gamal@gmail.com>
2022-05-28 15:48:04 +0200

    upddate Q5

--- app/Helpers/MysqlPreparedStatements.php
+++ app/Helpers/MysqlPreparedStatements.php
@@ -3,6 +3,8 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
+use \Log;
 use mysqli;
 
 class MysqlPreparedStatements
@@ -37,6 +39,7 @@ class MysqlPreparedStatements
      */
     public function whereStatment($table, array $selected_columns, array $where_conditions)
     {
+
         $table = strip_tags($table);
         $selected_columns = Arr::map($selected_columns, function ($item) {
             return strip_tags($item);
@@ -44,10 +47,21 @@ class MysqlPreparedStatements
         $where_conditions = Arr::map($where_conditions, function ($item) {
             return strip_tags($item);
         });
+
+        $cache_key = $this->getCacheKey($table, $selected_columns, $where_conditions);
+        $consider_cache = $this->considerCache($cache_key);
+        if ($consider_cache) {
+            $cache = $this->checkCache($cache_key);
+            if ($cache) {
+                return $cache;
+            }
+        }
+
         $selected_columns_string = implode(',', $selected_columns);
         $where_conditions_string = implode(' = ? ,', array_keys($where_conditions));
-        $sql_statement = 'SELECT ' . $selected_columns_string . ' FROM ' . $table .' '. $where_conditions_string;
-        $sql_statement = $this->conection->prepare($sql_statement);
+        $query_string = 'SELECT ' . $selected_columns_string . ' FROM ' . $table . ' ' . $where_conditions_string;
+        $sql_statement = $this->conection->prepare($query_string);
+
         foreach (array_values($where_conditions) as $value) {
             $type = $this->bind_types[gettype($value)];
             $sql_statement->bind_params($type, $value);
@@ -56,6 +70,52 @@ class MysqlPreparedStatements
         $sql_statement->execute();
         $result = $sql_statement->get_result();
         $rows = $result->fetch_assoc();
-        return collect($rows);
+        $result_collect = collect($rows);
+        $this->setToCache($cache_key, $result_collect);
+        return $result_collect;
     }
+
+
+    public function getCacheKey(string $table, $selected_columns, $where_conditions): string
+    {
+
+        $cache_key = trim($table) . implode($selected_columns, '_');
+        $where_conditions_string = '';
+        foreach ($where_conditions as $k => $v) {
+            $where_conditions_string .= (string)$k . '_' . (string)$v;
+        }
+        $cache_key = $cache_key . $where_conditions_string;
+
+        return $cache_key;
+
+    }
+
+    public function considerCache($cache_key)
+    {
+        //consider max key limit
+        $string_size = mb_strlen($cache_key);
+        if ($string_size > env('MAX_MEMCACHED_KEY_SIZE')) {
+            Log::info('cant cache : ' . $cache_key);
+            return false;
+        }
+        return true;
+    }
+
+    public function checkCache($cache_key)
+    {
+        if (Cache::store('memcached')->has($cache_key)) {
+            return Cache::store('memcached')->get($cache_key);
+        }
+        return false;
+    }
+
+    public function setToCache($cache_key, $value)
+    {
+        if ($this->considerCache($cache_key)) {
+            return Cache::store('memcached')->put($cache_key, $value, env('DEFAULT_QUERY_CACHE_TIME'));
+        }
+        return false;
+    }
+
+
 }
